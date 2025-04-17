04aa2f205a78b0ee7edccb067f98938785f0b6b3
Jesse Owen <144388209+jesseowenuk@users.noreply.github.com>
2025-04-17 22:23:04 +0100

    Updated countdown.asm to include spaces

--- experiments/countdown.asm
+++ experiments/countdown.asm
@@ -3,13 +3,17 @@
 
 [org 0x7C00]
 
-    mov cx, 10                   ; Put the number 9 into the CX register to represent the number of digits we want to print
+    mov ah, 0x0E                ; Enter the teletype text mode
+
+    mov cx, 10                  ; Put the number 9 into the CX register to represent the number of digits we want to print
     mov bl, 9                   ; Put the starting number into the BL register
 
 .loop:
     mov al, bl                  ; Copy the current number into the BL register
     add al, '0'                 ; Convert the current number to ASCII (e.g. 1 -> '1') by adding 48 (or '0')
-    mov ah, 0x0E                ; Enter the teletype text mode
+    int 0x10                    ; Call the BIOS interrupt 0x10 to print the current character
+
+    mov al, ' '                 ; Put a space after the letter
     int 0x10                    ; Call the BIOS interrupt 0x10 to print the current character
 
     dec bl                      ; decrement the BL register (subtract 1)
